042a3c26ecf40d6a8edf1e36965b9b7889ae8b9e
Anton Zaviyalov <zavanton123@gmail.com>
2019-10-20 18:39:02 +0300

    main layout ok

--- app/src/main/java/com/example/copyproject/MainActivity.kt
+++ app/src/main/java/com/example/copyproject/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.copyproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvSimple.setOnClickListener {
+            Toast.makeText(this, "Click", Toast.LENGTH_SHORT).show()
+        }
     }
 }
