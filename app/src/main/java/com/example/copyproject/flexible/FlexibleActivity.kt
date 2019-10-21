e58b03e2d9c17d45074c4587c81972bc8405f7e7
Anton Zaviyalov <zavanton123@gmail.com>
2019-10-21 16:26:23 +0300

    flexible demo one

--- app/src/main/java/com/example/copyproject/flexible/FlexibleActivity.kt
+++ app/src/main/java/com/example/copyproject/flexible/FlexibleActivity.kt
@@ -1,13 +1,31 @@
 package com.example.copyproject.flexible
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.copyproject.R
+import kotlinx.android.synthetic.main.activity_flexible.*
 
 class FlexibleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flexible)
+
+        setSupportActionBar(vToolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.simple_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.item_settings) {
+            Toast.makeText(this, "Click", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
